f5be9e5a60854b239a240bd107501a94680f4899
a1shuvo <a1shuvosaha@gmail.com>
2025-08-13 23:25:06 +0600

    Initial server setup with essential packages installed

---
+++ index.js
@@ -0,0 +1,36 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(
+  helmet({
+    crossOriginOpenerPolicy: false,
+  })
+);
+
+const PORT = 8080 || process.env.PORT;
+
+app.get("/", (request, response) => {
+  // Server to client
+  response.json({
+    message: "Server is running on " + PORT,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on ", PORT);
+});
